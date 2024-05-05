157656814f90c90d1ab8e7391abfca4a223fdb36
Nimrod Yizhar <nyizhar@gmail.com>
2024-05-05 11:26:38 +0300

    initial commit

---
+++ MaccabiExercise/MaccabiExercise/MaccabiExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaccabiExerciseApp.swift
+//  MaccabiExercise
+//
+//  Created by Nimrod Yizhar on 05/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MaccabiExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
